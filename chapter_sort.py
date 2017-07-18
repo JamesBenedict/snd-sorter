a8dfd44191551e41bffc793f6382d11fa3de48aa
James Benedict <bloop@Jamess-MBP.attlocal.net>
2017-07-18 18:55:58 -0400

    updated comments for steve

--- chapter_sort.py
+++ chapter_sort.py
@@ -1,18 +1,22 @@
-import re, csv, os
+import csv, os
 from shutil import copyfile
 from shutil import rmtree
 from glob import glob
 
+# gets current working directoy, this will save you time typing.
 path = os.getcwd() + '/'
 
+# grabs data from same directory as program file
 d = csv.DictReader(open('data.csv', 'r', encoding="utf-8",  errors ='replace'))
+
+# global variables 
 chapters = []
 subchapters = []
 chapdict = {}
 subdict = {}
 
 
-# utility 
+# utility to make numbers consistent, if '20' goes in '0020' comes out.
 def num_format(num):
 	if len(num) < 4:
 		zeros = 4 - int(len(num))
@@ -25,20 +29,26 @@ def num_format(num):
 def chap_dict():
 	for row in d:
 		# for each line the database writes its ref, chapter and subchapter into list for the program to use elsewhere 
+		
+		# below: new_variable = row_in_data['column_header_in_data']. 
 		ref = row['REF']
+		# returns a formatted number with the utility above
 		ref = num_format(ref)
 		chapter = row['chapter'] 
 		subchapter = row['subchapter']
 		
-		#creates a list of chapters 
+		#creates a list of chapters, 
+		#chapter is defined on each pass of the for loop, chapters is the global variable off all chapter
 		if chapter not in chapters:
+			# I store chapters in both a list and dictonary, the first line below appends the dict, the 2nd appends the list
 			chapdict[ref] = chapter
 			chapters.append(chapter)
-		# does the same thing for subchapters
+		# does the same thing for subchapters, the len statement is added to weed out subchaters with a length of 0 (blanks)
 		if subchapter not in subchapters and len(subchapter) > 0:
 			subdict[ref] = subchapter
 			subchapters.append(subchapter)
 		
+		# catches ref not sorted the first time
 		if ref not in chapdict:
 			chapdict[ref] = chapter
 
@@ -53,14 +63,15 @@ def create_directories():
 	if not os.path.exists(path + 'img/chapters/'):
 			os.makedirs(path + 'img/chapters/')
 
-	# creates the individual chapter folders
+	# creates the individual chapter folders, must run the chap_dict function first to get a list of chapters
 	for chapter in chapters:
 		if not os.path.exists(path + 'img/chapters/' + chapter):
 			os.makedirs(path + 'img/chapters/' + chapter)
 
-
+		# i is ued for the for loop, but would be erased if it were within it
 		i = 0
 		for subchapter in subchapters:
+			# makes folders for each subchapter
 			parent_chapter = subchapters[i][0:2]
 		# print(parent_chapter)
 			i += 1
@@ -69,49 +80,50 @@ def create_directories():
 			if chapter[0:2] == parent_chapter:
 				# print(chapter, parent_chapter)
 			# navigate into parent chapter
-		# if that parent doesn't have a folder called the subfolder
-		# make a folder for each subchapter where the parent chapter = current directory
-
-
+			# if that parent doesn't have a folder called the subfolder
+			# make a folder for each subchapter where the parent chapter = current directory
 				if not os.path.exists(path + 'img/chapters/' + chapter + '/' + subchapter ):
 			 		os.makedirs(path + 'img/chapters/' + chapter + '/' + subchapter)
-	# 	# chapter = subchapters[0][0:2]
 	# 	# print(chapter) 
-	# 	i += 1
-	# 	print(chapter)
 
 def move_imgs():
+	# Woooh now onto the actually fun part of sorting
+
+	# gathers all path to all the images held in the img/unsorted directory 
 	paths = glob('img/unsorted/*')
 
 	for path in paths:
+		# reference is grabbed from the name of the image
 		ref = path[13:-4]
+		# And then stripped of extra text like ''
 		ref_num = ref[0:4] 
 
 		if ref_num in subdict:
+			# first I want to sort images into the subfolders
+
+			# this looks ups from the dictonary, dicts are great for recall
 			chapter = chapdict[ref_num]
 			subchapter = subdict[ref_num]
+			# builds an input/output string that is used to sort the image
 			file = os.getcwd() + '/img/unsorted/' + ref + '.jpg'
 			output = os.getcwd() + '/img/chapters/' + chapter + '/' + subchapter + '/' + ref + '.jpg'
 			copyfile(file, output)
-
-
+			# a test that prints out in the terminal.
 			print(ref, 'moved')
 			pass
+
 		elif ref_num in chapdict:
-			# print(ref_num)
-			# print(ref)
-	
+			# same idea as above
 			chapter = chapdict[ref_num]
 			file = os.getcwd() + '/img/unsorted/' + ref + '.jpg'
 			output = os.getcwd() + '/img/chapters/' + chapter + '/' + ref + '.jpg'
+			print(ref, 'moved')
 			copyfile(file, output)
 		
 		else:
+			# a test to print out everything that didn't move
 			print(ref, 'did not move')
 
-
-		
-	
 chap_dict()
 create_directories()
 move_imgs()
--- img_sorter.py
+++ img_sorter.py
@@ -2,25 +2,27 @@ import re, csv, os
 from shutil import copyfile
 from shutil import rmtree
 from glob import glob
-# d = csv.reader(open('data.csv', "r", encoding="utf-8", errors ='replace'))
-# x = list(d)
-# for row in d:
-# 	print(row[0])
+
+# The chapter sorter is what is actually used
 
 path = os.getcwd() + '/'
-
-# publications = ['Comercio', 'National Geographic', 'Expresso', 'National Geographic Traveler', 'Centro', 'National Post', 'Wall Street Journal', 'New York Times', 'singles', 'New York Times Magazine', 'Beijing News Agency', 'Omaha World-Herald', 'Bergens Tidende', 'Parool', 'Berliner Morgenpost', 'Philippine Daily Inquirer', 'Bild Am Sonntag', 'Pittsburgh Post-Gazette', 'Boston Globe', 'Plain Dealer', 'Buffalo News', 'Politico Europe', 'Chicago Tribune', 'Politico Magazine', 'China Daily', 'Politiken', 'Clarin', 'Razon de Mexico', 'Colombiano', 'Revista Saude', 'Crains Cleveland Business', 'San Diego Union-Tribune', 'Dagens Nyheter', 'San Francisco Chronicle', 'Dallas Morning News', 'Shabiba', 'Denver Post', 'South China Morning Post', 'Excelsior', 'Stuttgarter Zeitung', 'Financiero', 'Sun Sentinel', 'Folha de Sao Paulo', 'Sunday', 'Galileau', 'Tampa Bay Times', 'Globe and Mail', 'Telegrafo', 'Guardian', 'Times of Oman', 'Gulf News', 'Variety', 'Honolulu Star Bulletin', 'Verdens Gang', 'LEQ Law Enforcement Quarterly', 'Villages Daily Sun', 'Los Angeles Times', 'Voz del Interior', 'Louisville Courier-Journal', 'Washington Business Journal', 'Metro Sao Paulo', 'Washington Post', 'Ming Pao Daily News', 'Welt', 'Minneapolis Star Tribune', 'Welt am Sonntag', 'Nacion', 'Zeit', 'National']
-# publications = ['(Colorado Springs) Gazette', '(Lima) Comercio', '(Lisbon) Expresso', '(London) Sunday Times', '(Mexico City) Centro', '(New York) Wall Street Journal', 'Arizona Republic', 'Battle Creek Enquirer', 'Beijing News Agency', 'Bergens Tidende', 'Berliner Morgenpost', 'Bild am Sonntag', 'Boston Globe', 'Buffalo News', 'Business Times', 'Chatelaine', 'Chicago Tribune', 'Chillicothe Gazette', 'China Daily', 'Chronicle of Higher Education', 'Clar�n', 'Colombiano', 'Columbiano', 'Crain�s Cleveland Business', 'Dagens Nyheter', 'Dallas Morning News', 'Denver Post', 'Di Weekend', 'Diario de Hoy', 'Economic Observer', 'Estado de Sao Paulo', 'Excelsior', 'Financiero', 'Folha de S�o Paulo', 'Galileu', 'Globe and Mail', 'Globo', 'Greenville News', 'Guardian', 'Gulf News', 'Helsingin Sanomaat', 'Honolulu Star-Bulletin', 'Indianapolis Star', 'LEQ Law Enforcement Quarterly', 'Los Angeles Times', 'Louisville Courier-Journal', 'Mercury News', 'Metro S�o Paulo', 'Ming Pao Daily News', 'Minneapolis Star Tribune', 'Minneapolis/St. Paul Business Journal', 'Mundo Estranho', 'Naci�n', 'National', 'National Geographic Magazine', 'National Geographic Traveler', 'National Post', 'New York Times', 'New York Times Magazine', 'News Tribune', 'Omaha World-Herald', 'Orlando Sentinel', 'Parool', 'Philippine Daily Inquirer', 'Pittsburgh Post-Gazette', 'Plain Dealer', 'Politico Europe', 'Politico Magazine', 'Politiken', 'Prensa Gr�fica', 'Prensa Libre', 'Provincia', 'Raz�n de M�xico', 'Reporte Indigo', 'Repubblica', 'Revista Sa�de', 'San Diego Union-Tribune', 'San Francisco Business Times', 'San Francisco Chronicle', 'Scenario Magazine', 'Science', 'Seattle Times', 'Shabiba', 'South China Morning Post', 'Stuttgarter Zeitung', 'Sun Sentinel', 'Sunday', 'Svenska Dagbladet', 'Sydney Morning Herald', 'Tagesspiegel', 'Tampa Bay Times', 'Tel�grafo', 'Tennessean', 'Times of Oman', 'Times Polska', 'Today', 'USA Today', 'Variety', 'Verdens Gang', 'Villages Daily Sun', 'Virginian-Pilot', 'Voz del Interior', 'Washington Business Journal', 'Washington Post', 'Welt', 'Welt am Sonntag', 'Zeit', 'Zhejiang Daily']
+# global variable used throughout program
 publications = []
-d = csv.DictReader(open('data.csv', 'r', encoding="utf-8",  errors ='replace'))
 pdict = {}
-# generates list of unique publications
+
+
+# grabs data
+d = csv.DictReader(open('data.csv', 'r', encoding="utf-8",  errors ='replace'))
+
 def unique_pubs():
+	# generates list of unique publications
 	for row in d:
 		if row['PUBLICATION'] not in publications:
 			publications.append(row['PUBLICATION']);
 
 def pub_dict():
+	# generates a dictonary with the publication and all reference numbers for it. 
+	# In english: it tells me all the images made by a publication
 	for row in d:
 		pub = row['PUBLICATION'] 
 		ref = row['REF']
@@ -36,8 +38,8 @@ def pub_dict():
 	return pdict
 
 
-# creates directories for unique pubs
 def create_directories():
+	# creates directories for unique pubs
 	os.makedirs(path + 'img/sorted/singles')
 
 	for paper in publications:
@@ -45,6 +47,7 @@ def create_directories():
 			os.makedirs(path + 'img/sorted/' + paper)
 
 def num_format(num):
+	# utility to make all numbers four digits, 1 becomes 0001
 	if len(num) < 4:
 		zeros = 4 - int(len(num))
 		num = str('0' * zeros) + num
@@ -56,6 +59,7 @@ def num_format(num):
 def move_imgs():
 
 	pub_dict()
+	# creates the dictonary, defined above
 
 	paths = glob('img/unsorted/*')
 	for path in paths:
@@ -69,56 +73,10 @@ def move_imgs():
 			print(file, output)
 			copyfile(file, output)
 
-
-
 		else:
 			print(ref_num, 'error moving')
 
 
-
-		# if ref_num[0:4] 
-		# # print(ref_num[0:4])
-		# # subdir_files = os.listdir(path)
-		# for key, value in pdict.items() :
-		# 	if ref_num[0:4] == value:
-		# 		print(key, value)
-		# 		print()
-
-				# pdict[key] = ref_num
-
-	# print(pub_dict())
-
-
-	# for row in d:
-	# 	folder = os.path.join(path +'img/sorted/' + row['PUBLICATION'])
-	# 		# code here
-	# 	os.chdir(folder)
-
-	# 	ref = str(row['REF'])
-	# 	ref = num_format(ref);
-	# 	# print(ref)
-	# 	try:
-	# 		file = path + '/img/unsorted/' + ref + '.jpg'
-	# 		output = os.getcwd() + '/' + ref + '.jpg'
-	# 		copyfile(file, output)
-	# 		nums.append(ref)
-
-	# 	except FileNotFoundError:
-	# 		try:
-	# 			# letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', '']
-	# 			# letters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-	# 			letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-	# 			for letter in letters:
-	# 				letter = letter.upper()
-	# 				file = path + '/img/unsorted/' + str(ref) + letter + '.jpg'
-	# 				output = os.getcwd() + '/' + str(ref) + letter + '.jpg'
-	# 				copyfile(file, output)
-	# 				nums.append(ref + letter)
-	# 		except:
-	# 			print(file, 'file not found')
-
-	# print(len(nums))
 
 def group_singles():
 	total_images =[ ]
